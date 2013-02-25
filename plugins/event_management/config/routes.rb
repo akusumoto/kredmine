230d11a1a80b192b2af61d1cbabde0656603d174
ituki_yu <ituki_yu@thanks-k.com>
2013-02-25 23:57:30 +0000

    ・partial => "" を削除。 ・routes.rb に、各モデルのresources追加。

--- plugins/event_management/config/routes.rb
+++ plugins/event_management/config/routes.rb
@@ -1,3 +1,6 @@
 RedmineApp::Application.routes.draw do
   match ':controller(/:action(/:id))(.:format)'
+	resources :event_models
+	resources :event_answer_datas
+	resources :event_user_answers
 end
