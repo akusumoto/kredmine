ac6c2c2f5caefb45de8e5c4e518361e1a21cbfb3
ituki_yu <ituki_yu@thanks-k.com>
2013-03-24 06:09:30 +0000

    全部に強制入力チェックを入れる。

--- plugins/event_management/app/models/event_model.rb
+++ plugins/event_management/app/models/event_model.rb
@@ -1,13 +1,27 @@
 class EventModel < ActiveRecord::Base
   unloadable
+	# 定義周り.
   has_many :event_answer_datas, :dependent => :destroy
   has_many :event_users, :dependent => :destroy
 	
+	# 入力チェック周り.
 	validates :event_subject,
 		:presence => true
 	validates :event_caption,
 		:presence => true
+	validates :project_id,
+		:presence => true
+	validates :event_owner_id,
+		:presence => true
+	validates :event_date,
+		:presence => true
+	validates :event_place_station,
+		:presence => true
+	validates :event_place,
+		:presence => true
+
 	
+	# 以下サポートメソッド的な.
 	def is_event_in_user( user_id )
 		ret = false
 		now_check_user_id = user_id.to_i
