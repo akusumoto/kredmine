ee27981f21d5a3ded7e76c470eb7c6b77ed22357
kusumoto <gkusumoto@gmail.com>
2014-05-31 15:36:38 +0000

    modified the event top page that divide future and past events

--- plugins/event_management/app/controllers/ctrl_event_top_controller.rb
+++ plugins/event_management/app/controllers/ctrl_event_top_controller.rb
@@ -11,10 +11,16 @@ class CtrlEventTopController < ApplicationController
   def index
 		session[:event] = nil
     # このプロジェクトに属する全イベントを取得する.
-		@now_order = "ASC"
-		order_field = "event_subject"
+		#@now_order = "ASC"
+		#order_field = "event_subject"
+		@now_order = "DESC"
+		order_field = "event_date"
 		setup_now_project_events(@now_order, order_field)
+		setup_past_project_events(@now_order, order_field)
 		@now_user = User.current
+
+		# for link of 'イベント日' on the view
+		@now_order = "ASC"
   end
 
 	def sort
@@ -23,6 +29,7 @@ class CtrlEventTopController < ApplicationController
 		@now_user = User.current
 		
 		if order_field == "event_answer_state" || order_field == "event_answer_count"
+			setup_past_project_events( @now_order, nil )
 			setup_now_project_events( @now_order, nil )
 			
 			if order_field == "event_answer_state"
@@ -41,11 +48,28 @@ class CtrlEventTopController < ApplicationController
 							b_ans <=> a_ans 
 						end
 					}
+					@past_project_events.sort!{ |a,b|
+						a_ans = 0
+						b_ans = 0
+						if a.is_answer_your( @now_user.id ) 
+							a_ans = a.get_user_answer( @now_user.id ).event_answer_data_id;	
+						end
+						if b.is_answer_your( @now_user.id )
+							b_ans = b.get_user_answer( @now_user.id ).event_answer_data_id;
+						end
+						if ( @now_order == "DESC" ) 
+							a_ans <=> b_ans 
+						else
+							b_ans <=> a_ans 
+						end
+					}
 			end
 		else 
+			setup_past_project_events( @now_order, order_field )
 			setup_now_project_events( @now_order, order_field )
 		end
 		
+		# for link of 'イベント日' on the view
 		if @now_order == "DESC"		
 			@now_order = "ASC"
 		else		
@@ -65,17 +89,21 @@ class CtrlEventTopController < ApplicationController
 #------------------------------------.
 # 以下ユーティリティ的なもの.
 #------------------------------------.
-	# このプロジェクトに属する全イベントを取得する.
+	# このプロジェクトに属する今後（当日を含む）のイベントを取得する.
 	def setup_now_project_events( now_order, order_field )
 		begin 
 			if ( order_field != nil && now_order != nil ) 
 				@now_project_events = EventModel.joins(:event_users).
+					where("event_date >= ?", Time.now.midnight).
                     where(:project_id => @project.id,
-                          'event_users.user_id' => User.current.id).order( " #{order_field} #{now_order}" ).uniq
+                          'event_users.user_id' => User.current.id).
+					order( " #{order_field} #{now_order}" ).uniq
 			else
                 @now_project_events = EventModel.joins(:event_users).
-                where(:project_id => @project.id,
-                      'event_users.user_id' => User.current.id).uniq
+					where("event_date >= ?", Time.now.midnight).
+                	where(:project_id => @project.id,
+                          'event_users.user_id' => User.current.id).
+					order(:event_date).uniq
 			end
 			this_user_id = User.current.id
 			
@@ -93,6 +121,37 @@ class CtrlEventTopController < ApplicationController
 		end
 	end
   
+	# このプロジェクトに属する過去（昨日以前）のイベントを取得する.
+	def setup_past_project_events( now_order, order_field )
+		begin 
+			if ( order_field != nil && now_order != nil ) 
+				@past_project_events = EventModel.joins(:event_users).
+					where("event_date < ?", Time.now.midnight).
+                    where(:project_id => @project.id,
+                          'event_users.user_id' => User.current.id).
+					order( " #{order_field} #{now_order}" ).uniq
+			else
+                @past_project_events = EventModel.joins(:event_users).
+					where("event_date < ?", Time.now.midnight).
+                	where(:project_id => @project.id,
+                          'event_users.user_id' => User.current.id).
+					order(:event_date).uniq
+			end
+			this_user_id = User.current.id
+			
+			@past_project_events_users = Hash.new
+			@past_project_events_users.store( this_user_id, User.current )
+			@past_project_events.each do |ev| 
+				begin
+					user = @project.principals.find(ev.event_owner_id)
+					@past_project_events_users.store( ev.event_owner_id, user)
+				rescue => e
+					p e.message
+				end
+			end
+		rescue ActiveRecord::RecordNotFound
+		end
+	end
 	def clear_data_all
 		#EventAnswerTable.destroy_all
 		EventModel.destroy_all
