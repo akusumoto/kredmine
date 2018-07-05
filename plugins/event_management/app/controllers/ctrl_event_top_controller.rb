96ff914f66df6929bf7a8b0d582c22c8536b330d
Akira Kusumoto <gkusumoto@gmail.com>
2018-07-05 23:19:06 +0900

    show other events which the user doesn't blong to for only admin user

--- plugins/event_management/app/controllers/ctrl_event_top_controller.rb
+++ plugins/event_management/app/controllers/ctrl_event_top_controller.rb
@@ -13,11 +13,14 @@ class CtrlEventTopController < ApplicationController
     # このプロジェクトに属する全イベントを取得する.
 		#@now_order = "ASC"
 		#order_field = "event_subject"
+		@now_user = User.current
 		@now_order = "DESC"
 		order_field = "event_date"
 		setup_now_project_events(@now_order, order_field)
 		setup_past_project_events(@now_order, order_field)
-		@now_user = User.current
+		if @now_user.admin?
+			setup_other_project_events(@now_order, order_field)
+		end
 
 		# for link of 'イベント日' on the view
 		@now_order = "ASC"
@@ -31,6 +34,9 @@ class CtrlEventTopController < ApplicationController
 		if order_field == "event_answer_state" || order_field == "event_answer_count"
 			setup_past_project_events( @now_order, nil )
 			setup_now_project_events( @now_order, nil )
+			if @now_user.admin?
+				setup_other_project_events( @now_order, nil )
+			end
 			
 			if order_field == "event_answer_state"
 					@now_project_events.sort!{ |a,b|
@@ -67,6 +73,9 @@ class CtrlEventTopController < ApplicationController
 		else 
 			setup_past_project_events( @now_order, order_field )
 			setup_now_project_events( @now_order, order_field )
+			if @now_user.admin?
+				setup_other_project_events( @now_order, order_field )
+			end
 		end
 		
 		# for link of 'イベント日' on the view
@@ -124,18 +133,33 @@ class CtrlEventTopController < ApplicationController
 	# このプロジェクトに属する過去（昨日以前）のイベントを取得する.
 	def setup_past_project_events( now_order, order_field )
 		begin 
+			# イベントを更新するたびに event_users が作られるバグあり
+			# event_users のレコードが重複してしまうため、
+			# しかたなく DISTINCT (uniq) している
+			@past_event_count = EventModel.joins(:event_users).
+					where("event_date < ?", Time.now.midnight).
+                    where(:project_id => @project.id, 'event_users.user_id' => User.current.id).
+					count('DISTINCT event_models.id, event_users.user_id')
+			@limit = per_page_option
+			@past_event_pages = Paginator.new self, @past_event_count, @limit, params['page']
+			@offset ||= @past_event_pages.current.offset
+
 			if ( order_field != nil && now_order != nil ) 
 				@past_project_events = EventModel.joins(:event_users).
 					where("event_date < ?", Time.now.midnight).
-                    where(:project_id => @project.id,
-                          'event_users.user_id' => User.current.id).
-					order( " #{order_field} #{now_order}" ).uniq
+                    where(:project_id => @project.id, 'event_users.user_id' => User.current.id).
+					order( " #{order_field} #{now_order}" ).
+					offset(@offset).
+					limit(@limit).
+					uniq
 			else
                 @past_project_events = EventModel.joins(:event_users).
 					where("event_date < ?", Time.now.midnight).
-                	where(:project_id => @project.id,
-                          'event_users.user_id' => User.current.id).
-					order(:event_date).uniq
+                	where(:project_id => @project.id, 'event_users.user_id' => User.current.id).
+					order(:event_date).
+					offset(@offset).
+					limit(@limit).
+					uniq
 			end
 			this_user_id = User.current.id
 			
@@ -152,6 +176,38 @@ class CtrlEventTopController < ApplicationController
 		rescue ActiveRecord::RecordNotFound
 		end
 	end
+
+	# このプロジェクトに属する今後（当日を含む）のイベントで自分が属さないものを取得する（管理者確認用）
+	def setup_other_project_events( now_order, order_field )
+		begin 
+			this_user_id = User.current.id
+			event_model_ids = EventUser.select(:event_model_id).where(:user_id => this_user_id)
+			if ( order_field != nil && now_order != nil ) 
+				@other_project_events = EventModel.where("id NOT IN (#{event_model_ids.to_sql})").
+					where("event_date >= ?", Time.now.midnight).
+                    where(:project_id => @project.id).
+					order( " #{order_field} #{now_order}" ).uniq
+			else
+				@other_project_events = EventModel.where("id NOT IN (#{event_model_ids.to_sql})").
+					where("event_date >= ?", Time.now.midnight).
+                	where(:project_id => @project.id).
+					order(:event_date).uniq
+			end
+			
+			@other_project_events_users = Hash.new
+			@other_project_events_users.store( this_user_id, User.current )
+			@other_project_events.each do |ev| 
+				begin
+					user = @project.principals.find(ev.event_owner_id)
+					@other_project_events_users.store( ev.event_owner_id, user)
+				rescue => e
+					p e.message
+				end
+			end
+		rescue ActiveRecord::RecordNotFound
+		end
+	end
+  
 	def clear_data_all
 		#EventAnswerTable.destroy_all
 		EventModel.destroy_all
